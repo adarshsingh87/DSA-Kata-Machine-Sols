35394ad7e8bf778772b25ed28b16c2e0d213e7d6
Adarsh singh <adarshmanoj.05.as@gmail.com>
2023-03-22 00:26:01 +0530

    feat: BT preorder

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,11 @@
 export default function pre_order_search(head: BinaryNode<number>): number[] {
-
+  if(!head.left) {
+    if (!head.right) return [head.value]
+    return [head.value, ...pre_order_search(head.right)]
+  }
+  if(!head.right){
+      if (!head.left) return [head.value];
+      return [head.value, ...pre_order_search(head.left)];
+  }
+  return [head.value, ...pre_order_search(head.left), ...pre_order_search(head.right)]  
 }
