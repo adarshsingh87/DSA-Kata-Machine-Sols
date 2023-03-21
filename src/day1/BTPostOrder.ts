ae1b3b7950df44946e3003181c67bd6c90311ea3
Adarsh singh <adarshmanoj.05.as@gmail.com>
2023-03-22 00:38:14 +0530

    feat: BT post order

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,8 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
-
+export default function post_order_search(head: BinaryNode<number> | null): number[] {
+    if(head===null) return []
+    return [
+        ...post_order_search(head.left),
+        ...post_order_search(head.right),
+        head.value,
+    ];
 }
