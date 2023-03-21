a42b866c555e0002ccfb3f2330567fe433c4fe3b
Adarsh singh <adarshmanoj.05.as@gmail.com>
2023-03-22 01:05:31 +0530

    feat: DFS on BST

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,6 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
-
+export default function dfs(head: BinaryNode<number> | null, needle: number): boolean {
+  if(!head) return false
+  if(head.value === needle) return true
+  if(head.value < needle) return false || dfs(head.right, needle)
+  return false || dfs(head.left, needle)
 }
