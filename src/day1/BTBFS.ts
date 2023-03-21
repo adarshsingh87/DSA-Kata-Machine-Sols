0b1af81d513f90ae5b0e2aeea0f1686d99dd2cfc
Adarsh singh <adarshmanoj.05.as@gmail.com>
2023-03-22 00:47:37 +0530

    feat: BT BFS

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,14 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    let q = [head];
 
+    while (q.length > 0) {
+        let next = q.shift();
+        if (next) {
+            if (next.value === needle) return true;
+            if(next.left ) q.push(next.left);
+            if(next.right) q.push(next.right);
+        }
+    }
+
+    return false
 }
