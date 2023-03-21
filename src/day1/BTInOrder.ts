1379db4020f71d36903ddad688a140e77eed480b
Adarsh singh <adarshmanoj.05.as@gmail.com>
2023-03-22 00:35:23 +0530

    feat: BT inorder

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,15 @@
 export default function in_order_search(head: BinaryNode<number>): number[] {
-
+    if (!head.left) {
+        if (!head.right) return [head.value];
+        return [head.value, ...in_order_search(head.right)];
+    }
+    if (!head.right) {
+        if (!head.left) return [head.value];
+        return [head.value, ...in_order_search(head.left)];
+    }
+    return [
+      ...in_order_search(head.left),
+      head.value,
+        ...in_order_search(head.right),
+    ];
 }
