41be206fcd923a6b3129e65af3e8c5131d98a3da
Adarsh singh <adarshmanoj.05.as@gmail.com>
2023-03-27 00:30:28 +0530

    fix: DFS Graph List

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,40 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(
+    graph: WeightedAdjacencyList,
+    atNode: number,
+    needle: number,
+    seen: boolean[],
+    path: number[],
+) {
+  if (seen[atNode]) return false;
+  
+  seen[atNode] = true;
 
+  path.push(atNode);
+  
+  if (atNode === needle) {
+      return true;
+  }
+
+    const graphAdj = graph[atNode];
+    for (let i = 0; i < graphAdj.length; i++) {
+        let edge = graphAdj[i];
+        if (walk(graph, edge.to, needle, seen, path)) {
+            return true;
+        }
+    }
+
+    path.pop();
+    return false;
+}
+
+export default function dfs(
+    graph: WeightedAdjacencyList,
+    source: number,
+    needle: number,
+): number[] | null {
+  let path: number[] = []
+  let seen = new Array(graph.length).fill(false)
+  walk(graph, source, needle, seen, path)
+  if(path.length) return path
+  return null
 }
