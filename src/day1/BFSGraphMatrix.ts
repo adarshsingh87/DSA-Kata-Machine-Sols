9fddf075762a9540a2d8021c6bd771443b09e3d8
Adarsh singh <adarshmanoj.05.as@gmail.com>
2023-03-27 00:19:31 +0530

    fix: BFS Graph Matrix

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,38 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+export default function bfs(
+    graph: WeightedAdjacencyMatrix,
+    source: number,
+    needle: number,
+): number[] | null {
+    let seen = new Array(graph.length).fill(false);
+    let prev = new Array(graph.length).fill(-1);
+    seen[source] = true;
+    let q = [source];
 
+    do {
+        let curr = q.shift() as number;
+
+        if (curr === needle) break;
+
+        let adgs = graph[curr];
+        for (let i = 0; i < adgs.length; i++) {
+            if (adgs[i] === 0) continue;
+            if (seen[i]) continue;
+            seen[i] = true;
+            prev[i] = curr;
+            q.push(i);
+        }
+        seen[curr] = true;
+    } while (q.length);
+
+    let curr = needle;
+    let out = [];
+    while (prev[curr] !== -1) {
+        out.push(curr);
+        curr = prev[curr];
+    }
+    if (out.length) {
+        out.push(source);
+        return out.reverse();
+    }
+    return null;
 }
