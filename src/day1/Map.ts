889ee625819a8e95ff70f32cb3eab1d75e5454b0
Adarsh singh <adarshmanoj.05.as@gmail.com>
2023-04-02 00:04:37 +0530

    feat: temp map solution

--- src/day1/Map.ts
+++ src/day1/Map.ts
@@ -1,21 +1,25 @@
-export default class Map<T extends (string | number), V> {
-    
-
-    
+export default class MyMap<T extends string | number, V> {
+    private localMap: Map<T, V>;
 
     constructor() {
+        this.localMap = new Map<T, V>();
     }
 
     get(key: T): V | undefined {
-
-}
+        return this.localMap.get(key);
+    }
     set(key: T, value: V): void {
-
-}
+        this.localMap.set(key, value);
+        return
+    }
     delete(key: T): V | undefined {
-
-}
+        let value = this.localMap.get(key)
+        this.localMap.delete(key)
+        return value
+    }
     size(): number {
-
-}
+        let ans = 0
+        this.localMap.forEach((_) => ans++)
+        return ans
+    }
 }
